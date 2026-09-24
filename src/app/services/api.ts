a6b5c5b04b052162d0955f63079ecf9cfe6b971d
Caio Santos <caiosantos.dev@outlook.com>
2026-09-24 08:46:26 -0300

    fix

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -3,7 +3,7 @@ import { environment } from '../../environments/environment';
 
 export const api = axios.create({
   baseURL: environment.apiUrl || 'http://localhost:5000',
-  timeout: 15000,
+  timeout: 120000,
 });
 
 api.interceptors.request.use(
